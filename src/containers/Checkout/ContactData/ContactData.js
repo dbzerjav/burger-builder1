311812f4382450870a083efca8cc6aef0d68b112
dan Z <dbzerjav@gmail.com>
2019-08-13 14:43:17 -0500

    add views and logig for signup and signin

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -144,7 +144,6 @@ class ContactData extends Component {
                 config: this.state.orderForm[key]
             });
         }
-
         let form = (
             <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
